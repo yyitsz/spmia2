b0a16d438c92897b0e7d3ec3dda262a309b38a8e
yyitsz <6728271+yyitsz@users.noreply.github.com>
2021-03-24 09:23:23 +0800

    chapter 4 - 1

--- licensing-service/src/main/java/com/thoughtmechanix/licenses/LicensesServiceApplication.java
+++ licensing-service/src/main/java/com/thoughtmechanix/licenses/LicensesServiceApplication.java
@@ -4,11 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class LicensesServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(LicensesServiceApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
--- organization-service/src/main/java/com/thoughtmechanix/organization/OrganizationApplication.java
+++ organization-service/src/main/java/com/thoughtmechanix/organization/OrganizationApplication.java
@@ -3,11 +3,12 @@ package com.thoughtmechanix.organization;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @SpringBootApplication
 @EnableEurekaClient
-//@EnableFeignClients
+@EnableFeignClients
 public class OrganizationApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrganizationApplication.class, args);
